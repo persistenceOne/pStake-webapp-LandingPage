e21c37ff7164d528a914e2e41989ddc71bfe4646
Raju Vemula <rj.vemula81@gmail.com>
2023-03-20 10:59:04 +0400

    Update config.ts

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -319,8 +319,8 @@ export const CosmosChains: CosmosChainData = {
   ],
   Mainnet: [
     {
-      rpc: "https://rpc.cosmoshub-4.audit.one/",
-      rest: "https://rest.cosmoshub-4.audit.one/",
+      rpc: "https://rpc.cosmos.audit.one",
+      rest: "https://rest.cosmos.audit.one",
       chainId: "cosmoshub-4",
       chainName: "Cosmos Hub",
       stakeCurrency: {
