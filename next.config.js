fa33e9c97d7f6b41a0ddc41f9acde2ee8a3b9c9e
Raju Vemula <rj.vemula81@gmail.com>
2023-09-12 11:48:22 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,10 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  }
 };
 
 module.exports = nextConfig;
