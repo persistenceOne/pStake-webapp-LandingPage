335d0364ba1cea179c53293140d1521d114b95da
blackpanther1881 <raju@persistence.one>
2023-09-12 16:51:31 +0530

    update sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 import { PageTemplate } from "../components/templates/template";
 import NetworkSection from "../components/organisms/networks";
+import Footer from "../components/organisms/Footer";
 
 const Home: NextPage = () => {
   return (
     <PageTemplate className="home" title="pSTAKE">
       <NetworkSection />
+        <Footer/>
     </PageTemplate>
   );
 };
